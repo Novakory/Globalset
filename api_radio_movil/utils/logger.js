1b1b73fff2c560c805752d1a5a3dbc87efcbb9fd
Novakory <novakory34@gmail.com>
2025-12-26 16:16:56 -0600

    Se corrigio para que los objetos los imprima correctamente en el log

--- api_radio_movil/utils/logger.js
+++ api_radio_movil/utils/logger.js
@@ -46,3 +46,15 @@ function formatArgs(args) {
     return arg;
   }).join(" ");
 }
+
+
+function safeStringify(obj) {
+  const seen = new WeakSet();
+  return JSON.stringify(obj, (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) return "[Circular]";
+      seen.add(value);
+    }
+    return value;
+  }, 2);
+}
